dde1ee38de22b9e39568a51fc15ec9f9b0d58143
RayChang <raychang861205@gmail.com>
2024-03-03 18:04:54 +0800

    prepare for button

--- Calculator/Views/Home.swift
+++ Calculator/Views/Home.swift
@@ -26,6 +26,9 @@ struct Home: View {
                         .foregroundColor(.white)
                 }
                 .padding()
+                
+                // MARK: Buttons
+                
             }
         }
     }
